9cf3bc3af4f373a46e2902bd149561efe7f883f8
DESKTOP-O9NK112\ShibOo <shaaban.altanany@gmail.com>
2019-06-16 12:31:08 +0200

    simple spring boot application from spring initializr

---
+++ SimpleSpringBootApplication/src/test/java/com/simpleSpring/SimpleSpringBootApplication/SimpleSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simpleSpring.SimpleSpringBootApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
